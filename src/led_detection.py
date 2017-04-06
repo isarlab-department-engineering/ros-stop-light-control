d53eeac29f5371f7faaa025e62e154782620f45d
Diego Tognelli <togne193@gmail.com>
2017-04-06 18:34:49 +0200

    added detection of green light

--- src/led_detection.py
+++ src/led_detection.py
@@ -17,10 +17,11 @@ HERE GOES ALL PARAMETERS
 purpleLower = numpy.array([120,240,50],dtype = "uint8")
 purpleUpper = numpy.array([145,255,155],dtype = "uint8")
 # green hsv treshold boundaries
-greenLower = numpy.array([170,204,60],dtype = "uint8")
-greenUpper = numpy.array([180,255,127],dtype = "uint8")
+greenLower = numpy.array([60,200,60],dtype = "uint8")
+greenUpper = numpy.array([70,255,155],dtype = "uint8")
 # treshold detection of mean value
-tresholdDetection = 10
+tresholdPurpleDetection = 10
+tresholdGreenDetection = 10
 # suppose you will always find lights in the low part of image
 # image have 160*120 resolution
 # treshold for crop image
@@ -36,10 +37,13 @@ ysectors = 10
 ysectordim = (xmax-xmin)/xsectors
 xsectordim = (ymax-ymin)/ysectors
 # ros variables
+# controlPublish = rospy.Publisher("redmask_detection_topic",String,queue_size=10)
+# debbuging topic
 croppedPublish = rospy.Publisher("cropped_image",Image,queue_size=10)
-notTassellatedPublish = rospy.Publisher("led_detector_not_tassellated",Image,queue_size=10)
-tassellatedPublish = rospy.Publisher("led_detector_tassellated",Image,queue_size=10)
-controlPublish = rospy.Publisher("redmask_detection_topic",String,queue_size=10)
+notTassellatedPurplePublish = rospy.Publisher("led_detector_purple_not_tassellated",Image,queue_size=10)
+notTassellatedGreenPublish = rospy.Publisher("led_detector_green_not_tassellated",Image,queue_size=10)
+tassellatedPurplePublish = rospy.Publisher("led_detector_purple_tassellated",Image,queue_size=10)
+tassellatedGreenPublish = rospy.Publisher("led_detector_green_tassellated",Image,queue_size=10)
 ''' 
 END PARAMETERS
 '''
@@ -64,24 +68,43 @@ def callback(data):
 	hsvImage = cv2.cvtColor(croppedImage,cv2.COLOR_BGR2HSV)
 	# generate red mask to test detection
 	purpleMask = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,purpleLower,purpleUpper))
-	notTassellatedPublish.publish(bridge.cv2_to_imgmsg(purpleMask))
-	# tassellation of calculated redmask 	
+	greenMask = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,greenLower,greenUpper))
+	notTassellatedPurplePublish.publish(bridge.cv2_to_imgmsg(purpleMask))
+	notTassellatedGreenPublish.publish(bridge.cv2_to_imgmsg(greenMask))
+	# tassellation of calculated purplemask 	
 	for y in range(ysectors):
 		for x in range(xsectors):
 			mean = numpy.mean(purpleMask[y*ysectordim:(y+1)*ysectordim,x*xsectordim:(x+1)*xsectordim])
 			for i in range(y*ysectordim,(y+1)*ysectordim):
 				for j in range(x*xsectordim,(x+1)*xsectordim):
 					purpleMask[i][j] = mean
-	# search red led in calulcated sectors
+	# tassellation of calculated greenmask 	
+	for y in range(ysectors):
+		for x in range(xsectors):
+			mean = numpy.mean(greenMask[y*ysectordim:(y+1)*ysectordim,x*xsectordim:(x+1)*xsectordim])
+			for i in range(y*ysectordim,(y+1)*ysectordim):
+				for j in range(x*xsectordim,(x+1)*xsectordim):
+					greenMask[i][j] = mean
+	# search purple led in calulcated sectors
 	for y in range(ysectordim/2,ymax-ymin,ysectordim):
 		for x in range(xsectordim,xmax-xmin,xsectordim):
-			 if(purpleMask[y][x][0] > tresholdDetection):
+			 if(purpleMask[y][x][0] > tresholdPurpleDetection):
 				rospy.loginfo("FOUND PURPLE")
 				break
 		else:
 			continue
 		break
-	tassellatedPublish.publish(bridge.cv2_to_imgmsg(purpleMask))
+	# search green led in calulcated sectors
+	for y in range(ysectordim/2,ymax-ymin,ysectordim):
+		for x in range(xsectordim,xmax-xmin,xsectordim):
+			 if(greenMask[y][x][0] > tresholdGreenDetection):
+				rospy.loginfo("FOUND GREEN")
+				break
+		else:
+			continue
+		break
+	tassellatedPurplePublish.publish(bridge.cv2_to_imgmsg(purpleMask))
+	tassellatedGreenPublish.publish(bridge.cv2_to_imgmsg(greenMask))
    except CvBridgeError as e:
 	print(e)
 	
