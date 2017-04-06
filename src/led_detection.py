979a61bc6427505b8543ac1698dd4d313d93e681
Diego Tognelli <togne193@gmail.com>
2017-04-06 16:39:13 +0200

    changed color detection from red to purple

--- src/led_detection.py
+++ src/led_detection.py
@@ -8,18 +8,17 @@ from cv_bridge import CvBridge, CvBridgeError
 # init cvbridge
 bridge = CvBridge()
 
-'''
+''' 
 HERE GOES ALL PARAMETERS 
 '''
-
 # stop light = purple
 # front light = green
 # purple hsv treshold boundaries
-rLower1 = numpy.array([0,204,60],dtype = "uint8")
-rUpper1 = numpy.array([10,255,127],dtype = "uint8")
+purpleLower = numpy.array([120,240,50],dtype = "uint8")
+purpleUpper = numpy.array([145,255,155],dtype = "uint8")
 # green hsv treshold boundaries
-rLower2 = numpy.array([170,204,60],dtype = "uint8")
-rUpper2 = numpy.array([180,255,127],dtype = "uint8")
+greenLower = numpy.array([170,204,60],dtype = "uint8")
+greenUpper = numpy.array([180,255,127],dtype = "uint8")
 # treshold detection of mean value
 tresholdDetection = 10
 # suppose you will always find lights in the low part of image
@@ -41,8 +40,7 @@ croppedPublish = rospy.Publisher("cropped_image",Image,queue_size=10)
 notTassellatedPublish = rospy.Publisher("led_detector_not_tassellated",Image,queue_size=10)
 tassellatedPublish = rospy.Publisher("led_detector_tassellated",Image,queue_size=10)
 controlPublish = rospy.Publisher("redmask_detection_topic",String,queue_size=10)
-
-'''
+''' 
 END PARAMETERS
 '''
 
@@ -65,27 +63,25 @@ def callback(data):
 	# convert in hsv format
 	hsvImage = cv2.cvtColor(croppedImage,cv2.COLOR_BGR2HSV)
 	# generate red mask to test detection
-	redMask1 = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,rLower1,rUpper1))
-	redMask2 = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,rLower2,rUpper2))
-	redMask = cv2.bitwise_or(redMask1,redMask2)
-	notTassellatedPublish.publish(bridge.cv2_to_imgmsg(redMask))
+	purpleMask = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,purpleLower,purpleUpper))
+	notTassellatedPublish.publish(bridge.cv2_to_imgmsg(purpleMask))
 	# tassellation of calculated redmask 	
 	for y in range(ysectors):
 		for x in range(xsectors):
-			mean = numpy.mean(redMask[y*ysectordim:(y+1)*ysectordim,x*xsectordim:(x+1)*xsectordim])
+			mean = numpy.mean(purpleMask[y*ysectordim:(y+1)*ysectordim,x*xsectordim:(x+1)*xsectordim])
 			for i in range(y*ysectordim,(y+1)*ysectordim):
 				for j in range(x*xsectordim,(x+1)*xsectordim):
-					redMask[i][j] = mean
+					purpleMask[i][j] = mean
 	# search red led in calulcated sectors
 	for y in range(ysectordim/2,ymax-ymin,ysectordim):
 		for x in range(xsectordim,xmax-xmin,xsectordim):
-			 if(redMask[y][x][0] > tresholdDetection):
-				rospy.loginfo("FOUND RED")
+			 if(purpleMask[y][x][0] > tresholdDetection):
+				rospy.loginfo("FOUND PURPLE")
 				break
 		else:
 			continue
 		break
-	tassellatedPublish.publish(bridge.cv2_to_imgmsg(redMask))
+	tassellatedPublish.publish(bridge.cv2_to_imgmsg(purpleMask))
    except CvBridgeError as e:
 	print(e)
 	
