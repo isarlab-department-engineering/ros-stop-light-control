6280b11bcfd5f19d66081718112ef5a7416d61ef
tanicar <andrifiore@gmail.com>
2017-04-06 12:06:32 +0200

    master: first commit

---
+++ src/led_detection.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import sys,rospy,cv2, numpy, roslib
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+bridge = CvBridge()
+# RED hsv treshold boundaries
+rLower1 = numpy.array([0,204,60],dtype = "uint8")
+rLower2 = numpy.array([170,204,60],dtype = "uint8")
+rUpper1 = numpy.array([10,255,127],dtype = "uint8")
+rUpper2 = numpy.array([180,255,127],dtype = "uint8")
+# QUALCOSALTRO hsv treshold boundaries
+
+# treshold detection of mean value
+tresholdDetection = 10
+# suppose you will always find lights in the low part of image
+# image have 160*120 resolution
+# treshold for crop image
+ymin = 70
+ymax = 120
+xmin = 30
+xmax = 130
+# variables for sectors
+# NOTE. xmax-xmin must be divisibile for xsector, same for ymax-ymin with ysector
+xsectors = 20
+ysectors = 10
+# dimension of sectors
+ysectordim = (xmax-xmin)/xsectors
+xsectordim = (ymax-ymin)/ysectors
+# ros variables
+croppedPublish = rospy.Publisher("cropped_image",Image,queue_size=10)
+notTassellatedPublish = rospy.Publisher("led_detector_not_tassellated",Image,queue_size=10)
+tassellatedPublish = rospy.Publisher("led_detector_tassellated",Image,queue_size=10)
+controlPublish = rospy.Publisher("redmask_detection_topic",String,queue_size=10)
+def detector():
+	rospy.init_node('led_detector',anonymous=True)
+	rospy.Subscriber("slow_image_topic",Image,callback)
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+
+def callback(data):
+   try:
+	rospy.loginfo(rospy.get_caller_id()+"Received an image")
+	# import cv image
+	cvImage = bridge.imgmsg_to_cv2(data)
+	# crop image 
+	croppedImage = cvImage[ymin:ymax,xmin:xmax]
+	croppedPublish.publish(bridge.cv2_to_imgmsg(croppedImage))
+	# convert in hsv format
+	hsvImage = cv2.cvtColor(croppedImage,cv2.COLOR_BGR2HSV)
+	# generate red mask to test detection
+	redMask1 = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,rLower1,rUpper1))
+	redMask2 = cv2.bitwise_and(hsvImage,hsvImage,mask=cv2.inRange(hsvImage,rLower2,rUpper2))
+	redMask = cv2.bitwise_or(redMask1,redMask2)
+	notTassellatedPublish.publish(bridge.cv2_to_imgmsg(redMask))
+	# tassellation of calculated redmask 	
+	for y in range(ysectors):
+		for x in range(xsectors):
+			mean = numpy.mean(redMask[y*ysectordim:(y+1)*ysectordim,x*xsectordim:(x+1)*xsectordim])
+			for i in range(y*ysectordim,(y+1)*ysectordim):
+				for j in range(x*xsectordim,(x+1)*xsectordim):
+					redMask[i][j] = mean
+	# search red led in calulcated sectors
+	for y in range(ysectordim/2,ymax-ymin,ysectordim):
+		for x in range(xsectordim,xmax-xmin,xsectordim):
+			 if(redMask[y][x][0] > tresholdDetection):
+				rospy.loginfo("FOUND RED")
+				break
+		else:
+			continue
+		break
+	tassellatedPublish.publish(bridge.cv2_to_imgmsg(redMask))
+   except CvBridgeError as e:
+	print(e)
+	
+if __name__ == '__main__':
+	try:
+		detector()
+	except rospy.ROSInterruptException:
+		pass
---
+++ src/led_emitter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+#import necessary libraries
+import sys, time, rospy
+import RPi.GPIO as gpio
+from std_msgs.msg import String
+
+#LED configuration
+RED_PIN		= 38
+GREEN_PIN	= 40
+BLUE_PIN	= 35
+
+def emitter():
+	# set up and initialize led 
+	gpio.setmode(gpio.BOARD)
+	gpio.setwarnings(False)
+	gpio.setup(RED_PIN,gpio.OUT)
+	gpio.setup(GREEN_PIN,gpio.OUT)
+	gpio.setup(BLUE_PIN,gpio.OUT)
+	# set up ros configurations
+	rospy.init_node('led_emitter',anonymous=True)
+	rospy.Subscriber("rospibot_network",String,callback)
+	# loop until ros shutdown
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+		print("Shutting down")
+
+def callback(data):
+	if(data.data == "w"):
+		rospy.loginfo("turn on led")
+		gpio.output(RED_PIN,gpio.HIGH) # turn on red
+	if(data.data == "x"):
+		rospy.loginfo("turn off led")
+		gpio.output(RED_PIN,gpio.LOW) # turn off red
+		gpio.output(GREEN_PIN,gpio.LOW)
+     	if(data.data == "a"):
+		gpio.output(GREEN_PIN,gpio.HIGH)            
+
+if __name__ == '__main__':
+	try:
+		emitter()
+	except rospy.ROSInterruptException:
+		pass
