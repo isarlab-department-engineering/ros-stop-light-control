0b26a52f8cd7341bb04ef18f2e085199c28eaff8
Diego Tognelli <togne193@gmail.com>
2017-04-11 15:45:06 +0200

    changed control node, now use motor hat node to drive motors

--- src/old_led_control.py
+++ src/old_led_control.py
@@ -5,41 +5,44 @@
 # this script will be implemented in another node 
 
 # import libraries
-import rospy,sys,time,atexit,numpy
+import rospy,sys,time,atexit
+import RPi.GPIO as gpio
 from std_msgs.msg import String,Int16MultiArray
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 # define variables
-avoidingVehicle = False
-array = Int16MultiArray()
-array.data = []
-controlPub = rospy.Publisher("cmd",Int16MultiArray,queue_size=1)
+flag = 0 # tonera utile lo so 
+mh = Adafruit_MotorHAT(addr=0x60)
+m1 = mh.getMotor(1)
+m2 = mh.getMotor(2)
 
 def turnOffMotors():
-	array.data = [0,0,0,0]
-	controlPub.publish(array)
+	m1.run(Adafruit_MotorHAT.RELEASE)
+	m2.run(Adafruit_MotorHAT.RELEASE)
 
 def setSpeed(motor1,motor2):
 	if motor1 == 0 and motor2 == 0:
 		turnOffMotors()
 	else:
-		array.data = [motor1,motor2,0,0]
-		controlPub.publish(array)
-	
+		m1.setSpeed(motor1)
+		m2.setSpeed(motor2)
+
 def avoidVehicle(): 
-	global avoidingVehicle
-	turnOffMotors()
-	avoidingVehicle = False 
+	turnOffMotors
+	flag = 0 
 
 def callback(data):
-	global avoidingVehicle
 	rospy.loginfo(rospy.get_caller_id() +" Led control String received: %s",data.data)
 	if data.data == "stop" :
 		turnOffMotors()
-	elif (data.data == "front" and avoidingVehicle == False):
-		avoidingVehicle = True
+	elif data.data == "front" and flag == 0:
 		avoidVehicle()
+		# until avoiding flag is high
+		flag = 1
 	elif data.data == "w":
 		setSpeed(150,150)
+		m1.run(Adafruit_MotorHAT.FORWARD)
+		m2.run(Adafruit_MotorHAT.FORWARD)
 	elif data.data == "s":
 		turnOffMotors()
 	
