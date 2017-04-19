78fa7bf0251c9cd68a38871df9e24c4990274e36
Diego Tognelli <togne193@gmail.com>
2017-04-19 17:53:38 +0200

    cahnged colors of led emitting for technical reasons

--- src/led_emitter.py
+++ src/led_emitter.py
@@ -6,32 +6,22 @@ import RPi.GPIO as gpio
 from std_msgs.msg import String
 
 # LED configuration
-FRONT_RED	= 19
-FRONT_GREEN	= 20
-FRONT_BLUE	= 21
-REAR_RED	= 12
-REAR_GREEN	= 13
-REAR_BLUE	= 16
-
-def turnOffGreen(int):
-
-def turnOnPurple(int):
-
-def turnOffPurple(int):
+FRONT_GREEN1	= 21
+FRONT_GREEN2	= 20
+REAR_BLUE1	= 19
+REAR_BLUE2	= 16
 
 def emitter():
 	# set up and initialize led 
 	gpio.setmode(gpio.BCM)
 	gpio.setwarnings(False)
-	gpio.setup(FRONT_RED,gpio.OUT)
-	gpio.setup(FRONT_GREEN,gpio.OUT)
-	gpio.setup(FRONT_BLUE,gpio.OUT)
-	gpio.setup(REAR_RED,gpio.OUT)
-	gpio.setup(REAR_GREEN,gpio.OUT)
-	gpio.setup(REAR_BLUE,gpio.OUT)
+	gpio.setup(FRONT_GREEN1,gpio.OUT)
+	gpio.setup(REAR_BLUE1,gpio.OUT)
+	gpio.setup(FRONT_GREEN2,gpio.OUT)
+	gpio.setup(REAR_BLUE2,gpio.OUT)
 	# set up ros configurations
 	rospy.init_node('led_emitter',anonymous=True)
-	rospy.Subscriber("rospibot_network",String,callback)
+	rospy.Subscriber("cmdinfo",String,callback)
 	# loop until ros shutdown
 	try:
 		rospy.spin()
@@ -39,16 +29,18 @@ def emitter():
 		print("Shutting down")
 
 def callback(data):
-	if(data.data == "w"): # turn on green and turn off purple when the car goes forward
+	if(data.data == "forward"): # turn on green and turn off purple when the car goes forward
 		rospy.loginfo("turn on green lights")
-		gpio.output(FRONT_GREEN,gpio.HIGH)
-		gpio.output(REAR_RED,gpio.LOW)
-		gpio.output(REAR_BLUE,gpio.LOW)
-	if(data.data == "x"): # turn off green and turn on purple when car stops
-		rospy.loginfo("turn on purple lights")
-		gpio.output(FRONT_GREEN,gpio.LOW)
-		gpio.output(REAR_RED,gpio.HIGH)
-		gpio.output(REAR_BLUE,gpio.HIGH)
+		gpio.output(FRONT_GREEN1,gpio.LOW)
+		gpio.output(FRONT_GREEN2,gpio.LOW)
+		gpio.output(REAR_BLUE1,gpio.HIGH)
+		gpio.output(REAR_BLUE2,gpio.HIGH)
+	if(data.data == "stop"): # turn off green and turn on purple when car stops
+		rospy.loginfo("turn on blue lights")
+		gpio.output(REAR_BLUE1,gpio.LOW)
+		gpio.output(REAR_BLUE2,gpio.LOW)
+		gpio.output(FRONT_GREEN1,gpio.HIGH)
+		gpio.output(FRONT_GREEN2,gpio.HIGH)
 
 if __name__ == '__main__':
 	try:
